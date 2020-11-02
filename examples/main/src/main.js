666a600e5b08db2a1dcd68a93201678a21fd43e4
wuweihui <wuweihui9833@dingtalk.com>
2020-11-02 16:13:30 +0800

    modified

--- examples/main/src/main.js
+++ examples/main/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 
+import './permission' // permission control
+
 Vue.config.productionTip = false
 
 new Vue({
--- examples/main/src/permission.js
+++ examples/main/src/permission.js
@@ -1,6 +1,6 @@
 import router from './router'
 import NProgress from 'nprogress' // progress bar
-import '@/components/NProgress/nprogress.less' // progress bar custom style
+import '@/components/NProgress/nprogress.css' // progress bar custom style
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
